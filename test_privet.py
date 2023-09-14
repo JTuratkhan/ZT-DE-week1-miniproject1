8cccd2c8a548fac2869002ccd1051790efd18412
JTuratkhan <108810678+JTuratkhan@users.noreply.github.com>
2023-09-14 00:10:01 -0400

    Update test_privet.py

--- test_privet.py
+++ test_privet.py
@@ -1,7 +1,7 @@
 from privet import privet, multiply, divide
 
-def test_privet():
-    assert privet("Elsa") == "Elsa"
+def test_privet(x):
+	assert privet(x) = x
 
 def test_multiply(x,y):
 	assert multiply(3,4) == 12
